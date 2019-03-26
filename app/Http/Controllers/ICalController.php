bdd269bdf43631e7e97f24bd00d70df2547923a3
Wouter Bles <whbles@gmail.com>
2019-03-26 14:08:02 +0100

    Add timezone to icalcontroller

--- app/Http/Controllers/ICalController.php
+++ app/Http/Controllers/ICalController.php
@@ -11,7 +11,7 @@ class ICalController extends Controller
     public function getAgendaItemsICalObject()
     {
         $agenda_items = AgendaItem::all();
-        define('ICAL_FORMAT', 'Ymd\THis\Z');
+        define('ICAL_FORMAT', 'Ymd\THis');
  
         $eol = "\r\n";
         $icalObject = "BEGIN:VCALENDAR" . $eol .
@@ -21,20 +21,37 @@ class ICalController extends Controller
         "X-WR-CALNAME:ESAC Agenda" . $eol .
         "DESCRIPTION:Een overzicht van alle ESAC activiteiten" . $eol .
         "X-WR-CALDESC:Een overzicht van alle ESAC activiteiten" . $eol .
-        "PRODID:-//Eindhovense Studenten Alpen Club//Agenda//NL"  . $eol;
+        "PRODID:-//Eindhovense Studenten Alpen Club//Agenda//NL"  . $eol .
+        "BEGIN:VTIMEZONE" . $eol .
+        "TZID:Europe/Amsterdam" . $eol .
+        "BEGIN:DAYLIGHT" . $eol .
+        "TZOFFSETFROM:+0100" . $eol .
+        "TZOFFSETTO:+0200" . $eol .
+        "DTSTART:19810329T020000" . $eol .
+        "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU" . $eol .
+        "TZNAME:CEST" . $eol .
+        "END:DAYLIGHT" . $eol .
+        "BEGIN:STANDARD" . $eol .
+        "TZOFFSETFROM:+0200" . $eol .
+        "TZOFFSETTO:+0100" . $eol .
+        "DTSTART:19961027T030000" . $eol .
+        "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU" . $eol .
+        "TZNAME:CET" . $eol .
+        "END:STANDARD" . $eol .
+        "END:VTIMEZONE" . $eol;
        
         // loop over events
         foreach ($agenda_items as $agenda_item) {
             $icalObject .=
             "BEGIN:VEVENT" . $eol .
-            "DTSTART:" . date(ICAL_FORMAT, strtotime($agenda_item->startDate)) . $eol .
-            "DTEND:" . date(ICAL_FORMAT, strtotime($agenda_item->endDate))  . $eol .
-            "DTSTAMP:" . date(ICAL_FORMAT, strtotime($agenda_item->created_at)) . $eol .
+            "DTSTART;TZID=Europe/Amsterdam:" . date(ICAL_FORMAT, strtotime($agenda_item->startDate)) . $eol .
+            "DTEND;TZID=Europe/Amsterdam:" . date(ICAL_FORMAT, strtotime($agenda_item->endDate))  . $eol .
+            "DTSTAMP;TZID=Europe/Amsterdam:" . date(ICAL_FORMAT, strtotime($agenda_item->created_at)) . $eol .
             "SUMMARY:" . $agenda_item->agendaItemTitle->text() . $eol .
             "DESCRIPTION:" . strip_tags($agenda_item->agendaItemText->text()) . $eol .
             "UID:" . $agenda_item->id . "@esac.nl" . $eol .
             "STATUS:CONFIRMED" . $eol .
-            "LAST-MODIFIED:" . date(ICAL_FORMAT, strtotime($agenda_item->updated_at)) . $eol .
+            "LAST-MODIFIED;TZID=Europe/Amsterdam:" . date(ICAL_FORMAT, strtotime($agenda_item->updated_at)) . $eol .
             "END:VEVENT" . $eol;
         }
  
