46c13cc0b49ee6fb240a3229dfe166780095edb9
Wouter Bles <whbles@gmail.com>
2019-03-27 20:24:46 +0100

    Add export button to agenda page

--- resources/lang/en/front-end/agenda.php
+++ resources/lang/en/front-end/agenda.php
@@ -12,6 +12,9 @@ return [
     "cantregister" => "Regestriation not possible",
     "info" => "Algemene informatie",
     'loginNeeded' => 'Please log in to register',
-    'needToBeLogdIn' => 'Please log in to register'
+    'needToBeLogdIn' => 'Please log in to register',
+    'export_agenda' => 'Export agenda',
+    'export_explanation' => 'By pressing one of the links below it is possible to synchronize all ESAC activities with your own agenda.',
+    'other' => 'Other',
 
 ];
--- resources/views/front-end/agenda.blade.php
+++ resources/views/front-end/agenda.blade.php
@@ -4,6 +4,40 @@
     {{--In this div is the agenda mounted by vue--}}
     <div id="agenda">
     </div>
+
+    <div id="ExportModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ trans('front-end/agenda.export_agenda')}}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>{{ trans('front-end/agenda.export_explanation') }}</p>
+                    <table class="table table-striped">
+                        <tr>
+                            <td class="align-middle">Apple</td>
+                            <td><a href="webcal://esac.nl/ical" class="btn btn-outline-primary btn-sm">Connect</a></td>
+                        </tr>
+                        <tr>
+                            <td class="align-middle">Google Calendar</td>
+                            <td><a href="https://calendar.google.com/calendar/r?cid=http://esac.nl/ical" class="btn btn-outline-primary btn-sm">Connect</a></td>
+                        </tr>
+                        <tr>
+                            <td class="align-middle">Windows + Outlook</td>
+                            <td><a href="webcal://esac.nl/ical" class="btn btn-outline-primary btn-sm">Connect</a></td>
+                        </tr>
+                        <tr>
+                            <td class="align-middle">{{ trans('front-end/agenda.other') }} (iCal URL)</td>
+                            <td>esac.nl/ical</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('scripts')
