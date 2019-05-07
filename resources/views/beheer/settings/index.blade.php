b674c436ecf40acb3db527826a8716664215c136
Wouter Bles <whbles@gmail.com>
2019-05-07 11:09:11 +0200

    Setting page now has the same layout as other management pages

--- resources/views/beheer/settings/index.blade.php
+++ resources/views/beheer/settings/index.blade.php
@@ -5,25 +5,34 @@
 @endsection
 
 @section('content')
-    <div class="col-12" >
+    <div class="row mb-3">
+        <div class="col-md-6">
+            <h1>{{trans("settings.settings")}}</h1>
+        </div>
 
-        <a href="{{ url('beheer/settings/edit') }}" class="btn btn-primary float-right" style="margin-bottom: 5px;">{{ trans('settings.edit') }}</a>
-
-        <table id="settings-table" class="table table-striped table-bordered">
-            <thead>
-                <th>{{ trans('settings.name') }}</th>
-                <th>{{ trans('settings.value') }}</th>
-            </thead>
-            <tbody>
-            @foreach($settings as $setting)
-                <tr>
-                    <td>{{trans('settings.' . $setting->name)}}</td>
-                    <td>
-                        {{$setting->value}}
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+        <div class="col-md-6">
+            <div class="btn-group mt-2 float-md-right" role="group" aria-label="Actions">
+                <a href="{{ url('beheer/settings/edit') }}" class="btn btn-primary">
+                    <span title="{{ trans('settings.edit') }}" class="ion-edit" aria-hidden="true"></span>
+                    {{ trans('settings.edit') }}
+                </a>
+            </div>
+        </div>
     </div>
+    <table id="settings-table" class="table table-striped table-bordered">
+        <thead>
+            <th>{{ trans('settings.name') }}</th>
+            <th>{{ trans('settings.value') }}</th>
+        </thead>
+        <tbody>
+        @foreach($settings as $setting)
+            <tr>
+                <td>{{trans('settings.' . $setting->name)}}</td>
+                <td>
+                    {{$setting->value}}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
