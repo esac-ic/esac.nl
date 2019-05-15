9ee1045000ac7f0e5253400c72f1914b105a4ce9
Indie <indiepeeters@hotmail.com>
2019-05-16 00:29:34 +0200

    Added Delete and Update tests

--- database/factories/NewsItemFactory.php
+++ database/factories/NewsItemFactory.php
@@ -2,11 +2,12 @@
 
 use Faker\Generator as Faker;
 use App\NewsItem;
+use App\Text;
 
 $factory->define(NewsItem::class, function (Faker $faker) {
     return [
-        'title' => $faker->realText,
-        'text' => $faker->realText,
+        'title' => factory(Text::class)->create(),
+        'text' => factory(Text::class)->create(),
         'image_url' =>$faker->imageUrl,
         'thumbnail_url' => $faker->imageUrl,
         'author' => $faker->firstName
