0ae2731d07065c377b5f70b10911de914bae58b6
Niek Hoffmans <niek@brandleeuw.nl>
2019-06-07 21:11:07 +0200

    Fixed reloading zekeringen after delete

--- resources/assets/vue/zekeringen/components/Zekering.vue
+++ resources/assets/vue/zekeringen/components/Zekering.vue
@@ -53,7 +53,9 @@
         ],
         methods: {
             deleteZekering(){
-                zekeringApi.deleteZekering(this.zekering.id);
+                zekeringApi.deleteZekering(this.zekering.id, this.zekeringDeleted);
+            },
+            zekeringDeleted(response) {
                 EventBus.$emit(constants.EVENT_RELOAD_ZEKERINGEN);
             },
             togleForm(){
