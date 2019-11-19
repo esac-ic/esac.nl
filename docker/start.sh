22677e3cd5da8333e8cebca3866ad8fd3dd43d32
otherwise777 <wouter.ligtenberg@ing.com>
2019-11-20 01:56:06 +0200

    updated lock files to fix docker-compose

--- docker/start.sh
+++ docker/start.sh
@@ -4,3 +4,4 @@ npm install
 npm run dev
 php artisan migrate:fresh --seed --env=docker
 php artisan serve --env=docker --host=0.0.0.0 --port=8000
+echo "als je hierboven geen foutmeldingen ziet kun je nu de esac test website vinden op http://127.0.0.1 veel plezier!"
