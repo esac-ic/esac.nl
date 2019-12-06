b4184facb7b37aad26579340fce480c9fa7852ee
Wouter (W.L.M.) Ligtenberg <wouter.ligtenberg@ing.com>
2019-12-06 15:29:40 +0200

    cleaned up branch

--- docker/start.sh
+++
@@ -1,7 +0,0 @@
-composer install
-#composer update
-npm install
-npm run dev
-php artisan migrate:fresh --seed --env=docker
-php artisan serve --env=docker --host=0.0.0.0 --port=8000
-echo "als je hierboven geen foutmeldingen ziet kun je nu de esac test website vinden op http://127.0.0.1 veel plezier!"
