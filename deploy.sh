27d3d7d10ff784c29e70adcb94925df02f45e6e1
Wouter (W.L.M.) Ligtenberg <wouter.ligtenberg@ing.com>
2019-12-17 00:33:21 +0200

    test deploy build

--- deploy.sh
+++ deploy.sh
@@ -14,13 +14,28 @@ fi
 #deploy to test server when brach is develop, deploy to prod server when branch is master
 if [ $TRAVIS_BRANCH == 'develop' ]
 then
-  ssh deploy@beta.esac.nl './update.sh website '$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG
+  ssh deploy@beta.esac.nl './update.sh website 0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG
 elif [ $TRAVIS_BRANCH == 'master' ]
 then
   ssh deploy@esac.nl './update.sh' #account is not setup yet
 fi
 
 #ssh deploy@beta.esac.nl './update.sh 0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG 0.0.25
+
+
+if [ $1 == 'website' ]
+then
+  ESAC_VERSION=$2
+  NGINX_VERSION=$(cat versions/nginx)
+elif [ $1 == 'nginx' ]
+then
+  ESAC_VERSION=$(cat versions/website)
+  NGINX_VERSION=$2
+else
+  sl -e
+fi
+
+
 
 
 
