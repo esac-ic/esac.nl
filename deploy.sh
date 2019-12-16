250240050623036eb11e2c6608d46cf661f12669
Wouter (W.L.M.) Ligtenberg <wouter.ligtenberg@ing.com>
2019-12-16 22:52:23 +0200

    updated branches to official staging branch

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #when push is being made to dev branch, run update.sh on dev server, if master run on prod server
 #creating the docker image, the tag represents
 SNAPSHOTTAG=''
-if [ $TRAVIS_BRANCH == 'docker_deploy_travis' ]
+if [ $TRAVIS_BRANCH == 'develop' ]
 then
   SNAPSHOTTAG='-staging'
 elif [ $TRAVIS_BRANCH == 'master' ]
@@ -12,9 +12,9 @@ else
 fi
 
 #deploy to test server when brach is develop, deploy to prod server when branch is master
-if [ $TRAVIS_BRANCH == 'docker_deploy_travis' ]
+if [ $TRAVIS_BRANCH == 'develop' ]
 then
-  ssh deploy@beta.esac.nl './update.sh 0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG 0.0.25
+  ssh deploy@beta.esac.nl './update.sh website '$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG
 elif [ $TRAVIS_BRANCH == 'master' ]
 then
   ssh deploy@esac.nl './update.sh' #account is not setup yet
