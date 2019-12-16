72711f0f5c8a5fd70a8122a9c7bba3db1d36ea65
Wouter (W.L.M.) Ligtenberg <wouter.ligtenberg@ing.com>
2019-12-17 01:52:05 +0200

    added commit date, fixed commit msg

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,18 @@ fi
 
 #get commiter name:
 AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
-COMMIT_MSG="$(git log -1 0a28b634470a505b74ad3ffda0fcdbcb0d6ccc94 --pretty="%s")"
+COMMIT_MSG="$(git log -1 $TRAVIS_COMMIT --pretty="%s")"
+COMMMIT_DATE="$(git log -1 $TRAVIS_COMMIT --pretty="%cD")"
+
 #deploy to test server when brach is develop, deploy to prod server when branch is master
 if [ $TRAVIS_BRANCH == 'develop' ]
 then
-  ssh deploy@beta.esac.nl './update.sh website 0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG '"'$AUTHOR_NAME'"' '"'$COMMIT_MSG'"'
+  ssh deploy@beta.esac.nl './update.sh website 0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG '"'$AUTHOR_NAME'"' '"'$COMMIT_MSG'"' '"'$COMMMIT_DATE'"'
 elif [ $TRAVIS_BRANCH == 'master' ]
 then
   ssh deploy@esac.nl './update.sh' #account is not setup yet
 fi
+
 
 #ssh deploy@beta.esac.nl './update.sh 0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG 0.0.25
 
