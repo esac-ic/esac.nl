35dee5f6d980f750a455d8d987cc4725a975bb76
Wouter (W.L.M.) Ligtenberg <wouter.ligtenberg@ing.com>
2019-12-29 13:59:28 +0100

    test storing private key in variable

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_705ea9aa11f9_key -iv $encrypted_705ea9aa11f9_iv -in deploy_key.enc -out deploy_key -d
-rm deploy-key.enc # Don't need it anymore
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
+#openssl aes-256-cbc -K $encrypted_705ea9aa11f9_key -iv $encrypted_705ea9aa11f9_iv -in deploy_key.enc -out deploy_key -d
+#rm deploy-key.enc # Don't need it anymore
+#chmod 600 deploy-key
+#mv deploy-key ~/.ssh/id_rsa
+
+echo $private_key_staging_server > ~/.ssh/id_rsa/deploy-key
+chmod 600 ~/.ssh/id_rsa/deploy-key
 
 #creating the docker image, the tag represents
 SNAPSHOTTAG=''
