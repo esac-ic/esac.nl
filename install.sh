da7ebbb2b059f15a851aefc5b5f0e7f67fdc506f
Wouter (W.L.M.) Ligtenberg <wouter.ligtenberg@ing.com>
2020-01-02 17:52:03 +0100

    setting back tests from test branch

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
 
-
-
+#get the original commit in case of a PR build
+ORIGINAL_COMMIT=$(git show --pretty=%P HEAD | head -1 | cut -d\  -f 2)
+ECHO $ORIGINAL_COMMIT
 #creating the docker image, the tag represents the environment
 SNAPSHOTTAG=''
-if [ $TRAVIS_PULL_REQUEST_BRANCH != '' ]
+if [[ $TRAVIS_PULL_REQUEST_BRANCH != '' ]]
 then
   SNAPSHOTTAG='-staging'
 elif [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST_BRANCH == '' ]]
@@ -27,15 +28,15 @@ then
   curl -X POST -H "Content-Type: application/json" -d \
   '{"state": "success",
   "target_url": "https://hub.docker.com/repository/docker/esac/website",
-  "description": "build and push successful",
+  "description": "build and push successful '$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG'",
   "context": "Docker image esac/website:0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG'"}' \
-  https://api.github.com/repos/esac-ic/esac.nl/statuses/$TRAVIS_COMMIT\?access_token\=$github_token_wouter
+  https://api.github.com/repos/esac-ic/esac.nl/statuses/$ORIGINAL_COMMIT\?access_token\=$github_token_wouter
 else
   curl -X POST -H "Content-Type: application/json" -d \
   '{"state": "failure",
   "target_url": "https://hub.docker.com/repository/docker/esac/website",
-  "description": "build or push unsuccessful",
+  "description": "build or push unsuccessful '$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG'",
   "context": "Docker image esac/website:0.0.'$TRAVIS_BUILD_NUMBER$SNAPSHOTTAG'"}' \
-  https://api.github.com/repos/esac-ic/esac.nl/statuses/$TRAVIS_COMMIT\?access_token\=$github_token_wouter
+  https://api.github.com/repos/esac-ic/esac.nl/statuses/$ORIGINAL_COMMIT\?access_token\=$github_token_wouter
 fi
 
