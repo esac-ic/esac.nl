0f17b6e0154db8cd2b9f33d804072d2c0910e4dd
Niek <niek@bytestack.nl>
2020-03-29 18:22:20 +0200

    fixed checkboxen and radio button required

--- resources/assets/vue/applicationForm/components/registrationForm/Elements/RegistrationRadioCheckboxField.vue
+++ resources/assets/vue/applicationForm/components/registrationForm/Elements/RegistrationRadioCheckboxField.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <div v-for="(option, index) in row.applicationFormRowOptions" style="margin-bottom: 5px">
-            <input :type="getType()" :name="row.id + '[]'" :value="option.value" :key="index" style="margin-right: 5px;">{{getName(option)}}
+            <input  :required="row.required"
+                    :type="getType()"
+                    :name="row.id + '[]'"
+                    :value="option.value"
+                    :key="index"
+                    style="margin-right: 5px;">
+            {{getName(option)}}
         </div>
     </div>
 </template>
