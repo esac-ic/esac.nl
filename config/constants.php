121c2e6fe48b7c9fb0ae9982794caca77b7543fa
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2023-07-06 17:50:10 +0200

    Updated constants.php to include a role for NSAC

--- config/constants.php
+++ config/constants.php
@@ -10,6 +10,7 @@ return [
     "Administrator" => 1,
     "Content_administrator" => 2,
     "Activity_administrator" => 3,
-    "Certificate_administrator" => 4
-
+    "Certificate_administrator" => 4,
+    "NSAC_emegency_info_administrator" => 5
+    
 ];
