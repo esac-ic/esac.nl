411bea4b473b2147751ce8cbf5730c42fff46553
mininick <nmg.verhoeven@gmail.com>
2023-07-17 13:03:34 +0200

    modified:   app/Http/Middleware/Authorize.php

--- app/Http/Middleware/Authorize.php
+++ app/Http/Middleware/Authorize.php
@@ -14,7 +14,7 @@ class Authorize
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $role,$role2,$role3 = "-1")
+    public function handle($request, Closure $next, $role,$role2="-1",$role3 = "-1")
     {
         if(Auth::user()->hasRole($role,$role2,$role3)){
             return $next($request);
