0f66f1af8e90c79cfcf3a1940e7c2fd3b4b8b00b
Wouter Bles <whbles@gmail.com>
2023-10-19 13:27:56 +0200

    Remove telescope

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
 
