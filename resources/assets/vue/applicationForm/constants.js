d743001c5618e65f8965ddc4e6196858a7506f51
Wouter Bles <whbles@gmail.com>
2023-10-25 17:37:54 +0200

    Fix form buttons

--- resources/assets/vue/applicationForm/constants.js
+++ resources/assets/vue/applicationForm/constants.js
@@ -8,13 +8,13 @@ export const FORM_TYPE_NUMBER = 'number';
 export const FORM_TYPE_TEXT = 'text';
 
 export const FORM_TYPES = {
-    [FORM_TYPE_TEXT]: "Text",
+    [FORM_TYPE_TEXT]: "Text (Single Line)",
     [FORM_TYPE_NUMBER]: "Number",
-    [FORM_TYPE_CHECK_BOX]: "Checkbox",
-    [FORM_TYPE_TEXT_BOX]: "TextBox",
-    [FORM_TYPE_SELECT]: "Select",
-    [FORM_TYPE_RADIO]: "Radio",
-    [FORM_TYPE_CHECK_BOXEN]: "Checkboxen"
+    [FORM_TYPE_CHECK_BOX]: "Checkbox ",
+    [FORM_TYPE_TEXT_BOX]: "Textbox (Multi Line)",
+    [FORM_TYPE_SELECT]: "Select (Dropdown)",
+    [FORM_TYPE_RADIO]: "Radio (Single Choice)",
+    [FORM_TYPE_CHECK_BOXEN]: "Checkbox (Multiple Choice)"
 };
 
 export const ROW_OPTION_FORM_TYPE = [
