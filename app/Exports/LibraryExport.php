67e5d18f392bab0c58ff1ab869d037268082e673
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-11-26 16:01:53 +0100

    removed comments

--- app/Exports/LibraryExport.php
+++ app/Exports/LibraryExport.php
@@ -3,8 +3,6 @@
 namespace App\Exports;
 
 use APP\Repositories\BookRepository;
-use Barryvdh\Debugbar\Facades\Debugbar;
-use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
@@ -37,19 +35,6 @@ class LibraryExport implements FromCollection, WithTitle, WithHeadings, ShouldAu
     /**
     * @return \Illuminate\Support\Collection
     */
-    // public function collection()
-    // {
-    //     $books = $this->bookRepository->all(['*']);
-    //     $exportData = [];
-    //     foreach($books as $book) {
-    //         //possible here to do some data filtering and manipulation etc, but isn't necesary at time of writing
-    //         $book->makeHidden('updated_at', 'deleted_at', 'created_at');
-    //         $data = $book->toArray();
-            
-    //         array_push($exportData, $data);
-    //     }
-    //     return new Collection($exportData);
-    // }
     public function collection()
     {
         return $this->bookRepository->all(["*"]);
@@ -63,14 +48,6 @@ class LibraryExport implements FromCollection, WithTitle, WithHeadings, ShouldAu
     public function headings(): array
     {
         return array_values(LibraryExport::PROPERTY_LABELS);
-        // return [
-        //     '#',
-        //     'Title',
-        //     'Year',
-        //     'Type',
-        //     'Country',
-        //     'Code',
-        // ];
     }
     
     /**
