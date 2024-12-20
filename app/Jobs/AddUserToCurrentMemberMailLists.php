e10914f44f67d6a1f6fd9afcb6714e56acf16d28
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-12-20 22:00:30 +0100

    Made the job that adds users to the mail lists specified in the current member mail lists setting

--- app/Jobs/AddUserToCurrentMemberMailLists.php
+++ app/Jobs/AddUserToCurrentMemberMailLists.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs;
 
+use App\CustomClasses\MailList\MailList;
+use App\CustomClasses\MailList\MailListFacade;
 use App\User;
 use Illuminate\Log;
 use Illuminate\Bus\Queueable;
@@ -21,7 +23,7 @@ class AddUserToCurrentMemberMailLists implements ShouldQueue
      * @return void
      */
     public function __construct(
-        public User $user
+        public User $user    
     )
     {
         //
@@ -32,10 +34,33 @@ class AddUserToCurrentMemberMailLists implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(MailListFacade $mailListFacade)
     {
         //get the value of the setting
-        $currentMemberMailLists = app(\App\Setting::SINGELTONNAME)->getSetting(\App\Setting::SETTING_CURRENT_MEMBER_MAIL_LISTS);
-        \Log::info("add " . $this->user->firstname . " to mailists: " . $currentMemberMailLists);
+        $currentMemberMailLists = explode(";", app(\App\Setting::SINGELTONNAME)->getSetting(\App\Setting::SETTING_CURRENT_MEMBER_MAIL_LISTS));
+        
+        //get all mail lists to check if 
+        $allMailLists = $mailListFacade->getAllMailLists();
+        $allMailListIds = [];
+        
+        //make a list of all mailist ids to check if the maillists we want to add the user to exist
+        foreach ($allMailLists as $mailList) {
+            array_push($allMailListIds, $mailList->getId());
+        }
+                
+        //for each of the active member lists: add as a member if it exists
+        foreach ($currentMemberMailLists as $mailList) {
+            $mailList = $mailList . env("MAIL_MAN_DOMAIN");
+
+            //check if the mailist exists and then add the user to the mail list
+            if (in_array($mailList, $allMailListIds)) {
+                $mailListFacade->addMember($mailList, $this->user->email, $this->user->getName());
+                \Log::info("add " . $this->user->firstname . " to mailists: " . $mailList);
+            } else {
+                //TODO: possibly display an error in the gui here somehow
+                \Log::error("tried adding user to maillist that doesn't exist");
+            }
+        }
+        
     }
 }
