21667bf0cadbfe694f1a134a9cb6a46f1281d043
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-12-21 23:15:36 +0100

    Added settings for the different member type mail lists

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -5,6 +5,12 @@ return [
     \App\Setting::SETTING_SHOW_INTRO_OPTION => 'Show intro package options (0 or 1)',
     \App\Setting::SETTING_FOOTER_PHONE_NR => 'Phone number in footer',
     \App\Setting::SETTING_PENDING_MEMBER_MAIL_LISTS => 'Pending member mail lists',
-    \App\Setting::SETTING_CURRENT_MEMBER_MAIL_LISTS => 'Current member mail lists',
+    \App\Setting::SETTING_NORMAL_MEMBER_MAIL_LISTS => 'Normal member mail lists',
+    \App\Setting::SETTING_EXTRAORDINARY_MEMBER_MAIL_LISTS => 'Extraordinary participant mail lists',
+    \App\Setting::SETTING_REUNIST_MEMBER_MAIL_LISTS => 'Reunist mail lists',
+    \App\Setting::SETTING_HONORARY_MEMBER_MAIL_LISTS => 'Honorary member mail lists',
+    \App\Setting::SETTING_MERIT_MEMBER_MAIL_LISTS => 'Member of service mail lists',
+    \App\Setting::SETTING_TRAINER_MEMBER_MAIL_LISTS => 'Trainer mail lists',
+    \App\Setting::SETTING_RELATIONSHIP_MEMBER_MAIL_LISTS => 'Relationship member mail lists',
     \App\Setting::SETTING_OLD_MEMBER_MAIL_LISTS => 'Old member mail lists',
 ];
