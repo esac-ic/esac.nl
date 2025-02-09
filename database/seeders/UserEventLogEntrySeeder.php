7a692486bbed31ed1bcc95c0f09bedd7d62f8d93
ThijsEgberts <thijssurft@gmail.com>
2025-02-09 20:01:43 +0100

    Made a very basic seeder

--- database/seeders/UserEventLogEntrySeeder.php
+++ database/seeders/UserEventLogEntrySeeder.php
@@ -13,11 +13,14 @@ class UserEventLogEntrySeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {        
-        $entry = new \App\Models\UserEventLogEntry();
-        $entry->eventType = UserEventTypes::MemberTypeChanged;
-        $entry->user_id = 1;
-        $entry->eventDetails = "changed to reunist";
-        $entry->save();
+    {   
+        for ($i = 0; $i < 10; $i++)
+        {
+            $entry = new \App\Models\UserEventLogEntry();
+            $entry->eventType = UserEventTypes::MemberTypeChanged;
+            $entry->user_id = 1;
+            $entry->eventDetails = "changed to reunist " . $i;
+            $entry->save();
+        }     
     }
 }
