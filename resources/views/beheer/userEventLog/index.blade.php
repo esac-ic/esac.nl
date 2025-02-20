9fdae9874e520e2d9d6f7b0cfc0bce7fb7f0543a
ThijsEgberts <thijssurft@gmail.com>
2025-02-20 21:56:16 +0100

    Hide username from event details table in index

--- resources/views/beheer/userEventLog/index.blade.php
+++ resources/views/beheer/userEventLog/index.blade.php
@@ -30,7 +30,7 @@
             <tr>
                 <th>Event type</th>
                 <th>Time</th>
-                <th>User</th>
+                <!-- <th>User</th> -->
                 <th>Event details</th>
                 <th>Management</th>
             </tr>
@@ -40,11 +40,11 @@
             <tr>
                 <td>{{$entry->eventType}}</td>
                 <td>{{$entry->created_at}}</td>
-                @if ($entry->user != null)
+                <!-- @if ($entry->user != null)
                     <td>{{$entry->user->getName()}}</td>
                 @else
                     <td>{{"Deleted User"}}</td>
-                @endif
+                @endif -->
                 <td>{{$entry->eventDetails}}</td>
                 
                 <td>
