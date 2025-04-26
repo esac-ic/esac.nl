3a983f04e2fdc1739a4464965d5116d4b16314ac
ThijsEgberts <thijssurft@gmail.com>
2025-04-26 18:34:31 +0200

    removed old code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -85,32 +85,3 @@ class UserFactory extends Factory
     }
     
 }
-
-// $factory->define(User::class, function (Faker $faker) {
-//     return [
-//         'email' => $faker->email,
-//         'password' => bcrypt($faker->password),
-//         'firstname' => $faker->firstName,
-//         'lastname' => $faker->lastName,
-//         'preposition' => $faker->word,
-//         'street' => $faker->streetName,
-//         'houseNumber' => $faker->randomNumber(),
-//         'city' => $faker->city,
-//         'zipcode' => $faker->postcode,
-//         'country' => $faker->country,
-//         'phonenumber' => $faker->phoneNumber,
-//         'phonenumber_alt' => $faker->phoneNumber,
-//         'emergencyNumber' => $faker->phoneNumber,
-//         'emergencystreet' => $faker->streetName,
-//         'emergencyHouseNumber' => $faker->randomNumber(),
-//         'emergencycity' => $faker->city,
-//         'emergencyzipcode' => $faker->postcode,
-//         'emergencycountry' => $faker->country,
-//         'birthday' => $faker->date(),
-//         'kind_of_member' => 'member',
-//         'IBAN' => $faker->iban(),
-//         'BIC' => $faker->bankAccountNumber,
-//         'incasso' => $faker->boolean,
-//         'remark' => $faker->sentence,
-//     ];
-// });
