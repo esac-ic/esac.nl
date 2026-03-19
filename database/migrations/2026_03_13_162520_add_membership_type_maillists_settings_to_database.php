9351922c7b819e8d413a4f99607e2810f0d87a74
indiePeeters <indieleopeeters@gmail.com>
2026-03-19 09:35:57 +0100

    Apply suggestion from @marijnvanderhorst
    Co-authored-by: Marijn van der Horst <github@marijnvanderhorst.nl>

--- database/migrations/2026_03_13_162520_add_membership_type_maillists_settings_to_database.php
+++ database/migrations/2026_03_13_162520_add_membership_type_maillists_settings_to_database.php
@@ -8,19 +8,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        $setting = new Setting(['name' => Setting::SETTING_NORMAL_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'alle-leden.esac.nl;nieuwsbrief.esac.nl']);
-        $setting->save();
-        $setting = new Setting(['name' => Setting::SETTING_EXTRAORDINARY_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'reunist.esac.nl;alle-leden.esac.nl;nieuwsbrief.esac.nl']);
-        $setting->save();
-        $setting = new Setting(['name' => Setting::SETTING_REUNIST_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'reunist.esac.nl']);
-        $setting->save();
-        $setting = new Setting(['name' => Setting::SETTING_HONORARY_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => '']);
-        $setting->save();
-        $setting = new Setting(['name' => Setting::SETTING_MERIT_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => '']);
-        $setting->save();
-        $setting = new Setting(['name' => Setting::SETTING_TRAINER_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'alle-leden.esac.nl;nieuwsbrief.esac.nl']);
-        $setting->save();
-        $setting = new Setting(['name' => Setting::SETTING_RELATIONSHIP_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => '']);
-        $setting->save();
+        Setting::insert([
+            ['name' => Setting::SETTING_NORMAL_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'alle-leden.esac.nl;nieuwsbrief.esac.nl'],
+            ['name' => Setting::SETTING_EXTRAORDINARY_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'reunist.esac.nl;alle-leden.esac.nl;nieuwsbrief.esac.nl'],
+            ['name' => Setting::SETTING_REUNIST_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'reunist.esac.nl'],
+            ['name' => Setting::SETTING_HONORARY_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => ''],
+            ['name' => Setting::SETTING_MERIT_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => ''],
+            ['name' => Setting::SETTING_TRAINER_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => 'alle-leden.esac.nl;nieuwsbrief.esac.nl'],
+            ['name' => Setting::SETTING_RELATIONSHIP_MEMBER_MAIL_LISTS, 'type' => Setting::TYPE_STRING, 'value' => ''],
+        ]);
     }
 };
