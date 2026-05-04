af9e36ae76ab7d176d4a9c9e320c13f54fe217bd
ThijsEgberts <thijssurft@gmail.com>
2026-05-04 14:30:30 +0200

    Add @property tags to eventLogEntry to prevent ide warnings

--- app/Models/UserEventLogEntry.php
+++ app/Models/UserEventLogEntry.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use App\Enums\UserEventTypes;
 use App\User;
 
+/**
+ * @property string $event_type
+ * @property string $event_details
+ */
 class UserEventLogEntry extends Model
 {
     use HasFactory;
