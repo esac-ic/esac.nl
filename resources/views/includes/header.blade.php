7925103418dc29467a908a643d481c96d7d35abd
jvbaast <janvanbaast@live.nl>
2026-09-07 12:29:38 +0200

    changed course page to alpine pages

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -29,7 +29,7 @@ var headerHome = [{
     background: 'header-img2.jpeg',
     heading: '{{'Ski mountaineering introduction'}}',
     message: '{{'Regularly, an introductory course to the ski mountaineering is organized during the winter week.'}}',
-    buttonUrl: '{{url('overview courses')}}',
+    buttonUrl: '{{url('alpenwinter')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img3.jpeg',
@@ -47,19 +47,19 @@ var headerHome = [{
     background: 'header-img5.jpeg',
     heading: '{{'Ski in the Alps'}}',
     message: '{{'During the carnival holiday the ESAC leaves travels a week to the Alps to enjoy the snow.'}}',
-    buttonUrl: '{{url('overview courses')}}',
+    buttonUrl: '{{url('alpenwinter')}}',
     buttonText: '{{'More Information'}}'
 },  {
     background: 'header-img7.jpeg',
     heading: '{{'Mountaineering courses'}}',
     message: '{{'The NSAC offers courses at various levels with the ultimate goal of making alpine trips independently.'}}',
-    buttonUrl: '{{url('overview courses')}}',
+    buttonUrl: '{{url('alpenzomer')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img8.jpeg',
     heading: '{{'Mountaineering'}}',
     message: '{{'Mountaineering is the king discipline in mountain sports where all facets come together.'}}',
-    buttonUrl: '{{url('overview courses')}}',
+    buttonUrl: '{{url('alpenzomer')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img9.jpeg',
