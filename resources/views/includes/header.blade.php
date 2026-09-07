e2c344b535260a566c88d19b7a70a7cff5ec4c7c
jvbaast <janvanbaast@live.nl>
2026-09-07 12:03:08 +0200

    quick fix with urls from website

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -29,37 +29,37 @@ var headerHome = [{
     background: 'header-img2.jpeg',
     heading: '{{'Ski mountaineering introduction'}}',
     message: '{{'Regularly, an introductory course to the ski mountaineering is organized during the winter week.'}}',
-    buttonUrl: '{{url('courses')}}',
+    buttonUrl: '{{url('overview courses')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img3.jpeg',
     heading: '{{'Climbing weekends in Belgium'}}',
     message: '{{'The ESAC annually organizes about 12 climbing weekends to Belgium, Germany or beyond.'}}',
-    buttonUrl: '{{url('klimweekenden')}}',
+    buttonUrl: '{{url('buitenklimmen')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img4.jpeg',
     heading: '{{'Climbing competition'}}',
     message: '{{'The EPlaKKa is held every year to determine who is the best climber of the ESAC.'}}',
-    buttonUrl: '{{url('eplakka')}}',
+    buttonUrl: '{{url('klimwedstrijden')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img5.jpeg',
     heading: '{{'Ski in the Alps'}}',
     message: '{{'During the carnival holiday the ESAC leaves travels a week to the Alps to enjoy the snow.'}}',
-    buttonUrl: '{{url('courses')}}',
+    buttonUrl: '{{url('overview courses')}}',
     buttonText: '{{'More Information'}}'
 },  {
     background: 'header-img7.jpeg',
     heading: '{{'Mountaineering courses'}}',
     message: '{{'The NSAC offers courses at various levels with the ultimate goal of making alpine trips independently.'}}',
-    buttonUrl: '{{url('courses')}}',
+    buttonUrl: '{{url('overview courses')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img8.jpeg',
     heading: '{{'Mountaineering'}}',
     message: '{{'Mountaineering is the king discipline in mountain sports where all facets come together.'}}',
-    buttonUrl: '{{url('courses')}}',
+    buttonUrl: '{{url('overview courses')}}',
     buttonText: '{{'More Information'}}'
 }, {
     background: 'header-img9.jpeg',
